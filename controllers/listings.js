8afc70e56edab84d9b4f76e739583bf913065568
hansika2623 <hansikamittal2623@gmail.com>
2025-03-25 15:50:59 +0530

    commit

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,7 +1,7 @@
 const Listing = require("../models/listing");
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapToken = process.env.MAP_TOKEN;
-const geocodingClient = mbxGeocoding({accessToken:mapToken})
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req,res)=>{
     const allListings = await Listing.find({});
@@ -36,7 +36,8 @@ module.exports.createListing = async(req,res,next)=>{
         query: req.body.listing.location,
         limit: 1,
       })
-      .send();
+        .send()
+        
       
     let url = req.file.path;
     let filename = req.file.filename;
@@ -44,11 +45,11 @@ module.exports.createListing = async(req,res,next)=>{
     const newListing = new Listing(req.body.listing);
     newListing.owner =  req.user._id;
     newListing.image = {url, filename};
-
+ 
     newListing.geometry = response.body.features[0].geometry;
-
-    let savedListing=await newListing.save();
+    let savedListing = await newListing.save();
     console.log(savedListing);
+
     req.flash("success","New Listing Created!");
     res.redirect("/listings");
 }
