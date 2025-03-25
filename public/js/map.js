a3d12050497d84d4c4d20eb9a2b56a80533d8b0b
hansika2623 <hansikamittal2623@gmail.com>
2025-03-25 15:50:59 +0530

    commit

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,16 @@
-mapboxgl.accessToken = mapToken;
 
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 9 // starting zoom
-});
+	mapboxgl.accessToken = mapToken;
+    const parsedCoordinates = JSON.parse(coordinates);
 
-const marker1 = new mapboxgl.Marker()
-        .setLngLat(coordinates)//Listing.geometry.coordinates
-        .addTo(map);
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: "mapbox://styles/mapbox/streets-v12",  //style URL
+        center: parsedCoordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        zoom: 9 // starting zoom
+    });
+   
+    const marker = new mapboxgl.Marker({color:'red'})
+    .setLngLat(parsedCoordinates)
+    .setPopup(new mapboxgl.Popup({offset: 25})
+    .setHTML(`<p>Exact location will be provided after booking!</p>`))
+    .addTo(map);
