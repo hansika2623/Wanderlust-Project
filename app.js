9da7fbcda48517e409dec800c9c7e1ea1a957822
hansika2623 <hansikamittal2623@gmail.com>
2025-03-27 18:14:14 +0530

    Removed .env and node_modules from Git tracking

--- app.js
+++ app.js
@@ -9,6 +9,9 @@
 //CLOUDINARY IS USED TO SAVE PHOTOS AT THE BACKEND
 //MAPBOX IS USED HERE TO INCLUDE MAPS
 //npm i @mapbox/mapbox-sdk
+//MONGO ATLAS & MONGO STORE is used for deployment
+//npm i connect-mongo; connect-mongo has by default 14days of time period, if user doesn't perfrm any activity within 14days then he is removed from the site.
+//Deployment is done with the help of render
 
 if(process.env.NODE_ENV != "production"){//it is only used in development phase and not in production phase
     require('dotenv').config();//This is due to the fact that .env file contains credentials which can't be shared and thus .env files are not uploaded on the github 
@@ -22,6 +25,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError= require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -31,7 +35,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main().then(()=>{
     console.log("Connected to the database")
@@ -40,7 +44,7 @@ main().then(()=>{
 });
 
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine","ejs");
@@ -51,8 +55,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const store = MongoStore.create({
+     mongoUrl : dbUrl,
+     crypto: {
+         secret: process.env.SECRET,
+     },
+     touchAfter: 24*3600,//touchAfter is used when we want if no updations are made and user is simply refreshing the page then no need to mske new store. The store will be refreshed after 24 hrs. It stores value in second therefore we multiplied it with 3600.
+ });
+
+ store.on("error", ()=>{
+     console.log("ERROR IN MONGO SESSION STORE", err);
+ })
+
 const sessionOptions = {
-    secret : "mysupersecretcode",
+    store,
+    secret : process.env.SECRET,
     resave : false,
     saveUninitialized : true,
     cookie: {
@@ -60,7 +77,7 @@ const sessionOptions = {
         //We can set the expiry date by using expires option. Here we have made the expiry date for the 7 days after the session is created.
         //Expires always counted in milli-seconds. Hence 7Days* 24 hrs* 60 minutes* 60seconds* 1000milliseconds
         maxAge : 7*24*60*60*1000,
-        httpOnly : true//to prevent cross-scripting attacks
+        httpOnly : true, //to prevent cross-scripting attacks
     },
 };
 
@@ -70,6 +87,13 @@ const sessionOptions = {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+// Middleware to pass Mapbox token to frontend
+app.use((req, res, next) => {
+    res.locals.mapToken = process.env.MAP_TOKEN; 
+    next();
+});
+
 
 app.use(passport.initialize());
 app.use(passport.session());
